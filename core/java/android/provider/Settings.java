3a4a72037391e9f82f2f767bfdbd0515d9123503
0xD34D <clark@scheffsblend.com>
2013-09-18 19:23:24 -0700

    Merged Active Display from ChameleonOS
    Active display: Add options for date and am/pm [1/2]
    
    Change-Id: Idc09772829cc6aa9bcfb064459fb3557c7b37f6b
    
    Conflicts:
    
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/res/values/donottranslate.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2687,6 +2687,18 @@ public final class Settings {
 	// - Active Display Chameleonn OS 
 
         /**
+         * Whether to display AM/PM after time when in 12h format
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_SHOW_AMPM = "active_display_show_ampm";
+
+        /**
+         * Whether to display the date above the time
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_SHOW_DATE = "active_display_show_date";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
